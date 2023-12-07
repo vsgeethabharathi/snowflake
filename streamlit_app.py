7f68e9fbbc859da2ba9a4d91a45c59d196268870
vsgeethabharathi <116265457+vsgeethabharathi@users.noreply.github.com>
2023-12-07 18:53:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-
 streamlit.title('Test code')
-
 streamlit.header('Happy Evening')
 streamlit.text('07-Dec-2023')
 streamlit.text('Snowflake learning')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
