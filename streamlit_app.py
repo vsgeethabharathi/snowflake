33a5453dd103ddb29d54ff26a02856a41c7e9b29
vsgeethabharathi <116265457+vsgeethabharathi@users.noreply.github.com>
2023-12-07 17:20:58 +0530

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('Test code')
 
-stremlit.header('Happy Evening')
+streamlit.header('Happy Evening')
 streamlit.text('07-Dec-2023')
 streamlit.text('Snowflake learning')
