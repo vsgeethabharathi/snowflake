8d7d6e7ee8ae5ce619ec6827164fcdee6ac2fb7a
vsgeethabharathi <116265457+vsgeethabharathi@users.noreply.github.com>
2023-12-07 18:54:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ streamlit.text('Snowflake learning')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
